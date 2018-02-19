5f7d4d57ffa1b14b638d94206cc14edf636a30c9
sanmin <lucanidae88@gmail.com>
2018-02-19 16:21:23 +0900

    test
    test

--- secret/secret_config.js
+++ secret/secret_config.js
@@ -8,6 +8,11 @@ var federation = {
         'clientID': '1623452237717047',
         'clientSecret': 'e24c87ec5bd70e7c757aed8a8773a540',
         'callbackURL': "http://localhost:3000/login/facebook/callback"
+    },
+    'kakao': {
+        'clientID': '9555a9531d508825f92242da00edefaf',
+        'clientSecret': '',
+        'callbackURL': "http://localhost:3000/login/kakao/callback"
     }
 };
 
