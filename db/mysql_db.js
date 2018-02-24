db3830b1787f213312c649b3e666f031916b85df
Junhyung <jkunivjun@gmail.com>
2018-02-24 23:00:28 +0900

    s
    s

--- db/mysql_db.js
+++ db/mysql_db.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var options = {
     host: 'localhost',
     user: 'root',
-    password: '923601',
-    database: 'node_express_first_db'
+    password: '1099as',
+    database: 'univjundb'
 };
 
 
@@ -13,3 +13,11 @@ var dbConn = mysql.createConnection(options);
 module.exports = dbConn;
 
 //hello2
+//univjundb
+//1099as
+//
+// password: '923601',
+// database: 'node_express_first_db'
+
+    // password: '1099as',
+    // database: 'univjundb'
